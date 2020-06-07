ce57329f5d635b9fddfaaf56f483fdd262885123
Gabriel Garcia <garciat@uber.com>
2020-06-07 12:17:40 +0200

    Implement ParseFailure formatting

--- validation/src/main/java/com/garciat/functional/parser/ParseFailure.java
+++ validation/src/main/java/com/garciat/functional/parser/ParseFailure.java
@@ -1,18 +1,20 @@
 package com.garciat.functional.parser;
 
+import com.garciat.functional.functions.Getter;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+
+import java.util.function.Function;
 
 public abstract class ParseFailure {
 
   private ParseFailure() {}
 
-  // formatting
-
-  public String format() {
-    // TODO
-    return toString();
+  public final String format() {
+    return format(new Formatter());
   }
+
+  protected abstract String format(Formatter formatter);
 
   // constructors
 
@@ -30,6 +32,14 @@ public abstract class ParseFailure {
     return new Tag(tag, sub);
   }
 
+  public static ParseFailure type(Class<?> type, ParseFailure sub) {
+    return new Type(type, sub);
+  }
+
+  public static ParseFailure getter(Getter<?, ?> getter, ParseFailure sub) {
+    return new Field(getter, sub);
+  }
+
   public static ParseFailure merge(ParseFailure left, ParseFailure right) {
     return new Merge(left, right);
   }
@@ -44,6 +54,10 @@ public abstract class ParseFailure {
     @Override
     public String toString() {
       return "ParseFailure.message(\"" + message + "\")";
+    }
+
+    protected String format(Formatter formatter) {
+      return formatter.line(message);
     }
   }
 
@@ -60,6 +74,14 @@ public abstract class ParseFailure {
       }
       return s + ")";
     }
+
+    protected String format(Formatter formatter) {
+      String s = "exception=" + throwable.getClass().getTypeName();
+      if (throwable.getMessage() != null) {
+        s += " message=" + throwable.getMessage();
+      }
+      return formatter.line(s);
+    }
   }
 
   @Value
@@ -72,17 +94,76 @@ public abstract class ParseFailure {
     public String toString() {
       return "ParseFailure.tag(\"" + tag + "\", " + sub + ")";
     }
+
+    protected String format(Formatter formatter) {
+      return formatter.line(tag)
+              + formatter.indented(sub::format);
+    }
+  }
+
+  @Value
+  @EqualsAndHashCode(callSuper=false)
+  public static class Type extends ParseFailure {
+    Class<?> type;
+    ParseFailure sub;
+
+    @Override
+    public String toString() {
+      return "ParseFailure.type(" + type + ", " + sub + ")";
+    }
+
+    protected String format(Formatter formatter) {
+      return formatter.line("Parsing type " + type.getName())
+              + formatter.indented(sub::format);
+    }
+  }
+
+  @Value
+  @EqualsAndHashCode(callSuper=false)
+  public static class Field extends ParseFailure {
+    Getter<?, ?> getter;
+    ParseFailure sub;
+
+    @Override
+    public String toString() {
+      return "ParseFailure.field(" + getter.getInfo() + ", " + sub + ")";
+    }
+
+    protected String format(Formatter formatter) {
+      return formatter.line("Parsing field " + getter.getInfo().getImplMethodName())
+              + formatter.indented(sub::format);
+    }
   }
 
   @Value
   @EqualsAndHashCode(callSuper=false)
   public static class Merge extends ParseFailure {
-    ParseFailure left;
-    ParseFailure right;
+    ParseFailure left, right;
 
     @Override
     public String toString() {
       return "ParseFailure.merge(" + left + ", " + right + ")";
+    }
+
+    protected String format(Formatter formatter) {
+      return left.format(formatter) + right.format(formatter);
+    }
+  }
+
+  public static final class Formatter {
+    public static final int INDENT_STEP = 2;
+    private final int indent;
+    private Formatter() {
+      this(0);
+    }
+    private Formatter(int indent) {
+      this.indent = indent;
+    }
+    public <T> T indented(Function<Formatter, T> cont) {
+      return cont.apply(new Formatter(indent + INDENT_STEP));
+    }
+    public String line(String s) {
+      return new String(new char[indent]).replace("\0", " ") + s + "\n";
     }
   }
 }
